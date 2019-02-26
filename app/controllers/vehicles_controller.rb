07eda010485c79f3aaf0cbbc82127657d4f96670
jason kim <jkim9348@gmail.com>
2019-02-26 14:12:15 +0100

    add vehicle form finished and link

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -7,4 +7,24 @@ class VehiclesController < ApplicationController
     @vehicle = Vehicles.find(params[:id])
   end
 
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    @vehicle.user = current_user
+    if @vehicle.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:photo, :description, :city, :price_day, :category)
+  end
+
 end
