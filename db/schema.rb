da679fdf8b313d30df8d2cb20a4242e2d50972eb
jason kim <jkim9348@gmail.com>
2019-02-27 13:59:24 +0100

    added booking index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_172530) do
+ActiveRecord::Schema.define(version: 2019_02_27_112522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_172530) do
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.string "status", default: "pending"
     t.bigint "user_id"
     t.bigint "vehicle_id"
     t.datetime "created_at", null: false
